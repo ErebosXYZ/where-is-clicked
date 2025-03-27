630150c37dc44ab95beef533cc4bd204d6f0791f
ErebosXYZ <robertmaynou@gmail.com>
2025-03-27 20:34:56 +0100

    done

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // Recuerda todas las propiedades que tiene el objeto event !
 
+
+// Haig de fer que quan es cliqui sobre la imatge del mapa es dibuixi la creu (line1 i line2). Creem la variable map
+
+let map = document.querySelector(".map");
+
+
+
 function drawCross(x, y) {
   // Creamos un elemento div para la primera línea
   const line1 = document.createElement("div");
@@ -21,3 +28,17 @@ function drawCross(x, y) {
   line2.style.top = y + "px";
   document.body.appendChild(line2);
 }
+
+// Hem de crear una funció que executi drawCross en el punt que cliquem del mapa.
+// Creem dues variables que guardin les posicions del click en el moment de l'event (e.clientX, e.clientY)
+// Cridem la funció drawCross amb els valors de x i y en el moment del click.
+
+map.addEventListener("click", function(e){
+  let x = e.clientX;
+  let y = e.clientY;
+  console.log(x, y);
+  drawCross(x, y);
+});
+
+
+
